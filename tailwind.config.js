330d9e30f56f448cb6275f29c2d53a9de407cf0c
Vasant <vasantp20@gmail.com>
2021-07-25 23:35:33 +0530

    Dropdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'chev-down': "url('./_icons/cheveron_down.svg')",
+       })
+    },
   },
   variants: {
     extend: {},
